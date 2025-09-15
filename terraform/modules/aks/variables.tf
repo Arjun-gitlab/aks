d13acaaa025963b49cb9fa42e265579d5d4a8311
Arjun-gitlab <mkr4gkr@gmail.com>
2025-09-15 11:43:46 +0530

    Update variables.tf

--- terraform/modules/aks/variables.tf
+++ terraform/modules/aks/variables.tf
@@ -4,6 +4,9 @@ variable "location" {}
 variable "private_subnet_id" {}
 variable "node_count" { default = 2 }
 variable "node_vm_size" { default = "Standard_B2s" }
-variable "node_zones" { type = list(string) default = ["1","2"] }
+variable "node_zones" {
+  type    = list(string)
+  default = ["1", "2"]
+}
 variable "log_analytics_workspace_id" {}
 variable "acr_id" {}
