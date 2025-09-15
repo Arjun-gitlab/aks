26c8640bd0087519a520a8f4fb2f6f336894ba5c
Arjun-gitlab <mkr4gkr@gmail.com>
2025-09-15 15:47:15 +0530

    Update main.tf

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -11,31 +11,28 @@ resource "azurerm_kubernetes_cluster" "aks" {
     node_count      = var.node_count
     vm_size         = var.node_vm_size
     vnet_subnet_id  = var.private_subnet_id
-    node_zones      = var.node_zones   # e.g. ["1","2"]
+    zones      = var.node_zones  
   }
  
   identity {
     type = "SystemAssigned"
   }
  
-  role_based_access_control {
-    enabled = true
-  }
+  role_based_access_control_enabled = true
  
   network_profile {
     network_plugin = "azure"
     load_balancer_sku = "standard"
     service_cidr = "10.1.0.0/16"
     dns_service_ip = "10.1.0.10"
-    docker_bridge_cidr = "172.17.0.1/16"
   }
  
-  addon_profile {
-    oms_agent {
-      enabled                    = true
-      log_analytics_workspace_id = var.log_analytics_workspace_id
-    }
+ oms_agent {
+    enabled = true
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.insights.id
   }
+
+  azure_policy_enabled = true
 }
  
 # Allow AKS kubelet identity to pull from ACR
