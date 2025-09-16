0336880651657d9f414286b7ff6de96061fd4ec8
Arjun-gitlab <mkr4gkr@gmail.com>
2025-09-16 11:10:49 +0530

    Update main.tf

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -46,7 +46,7 @@ module "aks" {
   private_subnet_id   = module.network.private_subnet_1_id
   node_count          = var.node_count
   node_vm_size        = var.node_vm_size
-  node_zones          = ["1","2"]
+  node_zones          = [3]
   log_analytics_workspace_id = azurerm_log_analytics_workspace.insights.id
   acr_id              = module.acr.acr_login_server != "" ? module.acr.acr_login_server : null
 }
