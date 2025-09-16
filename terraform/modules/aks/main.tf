c6f5c11e1d3b602c1d8d93f48d0b2953ebeb4b06
Arjun-gitlab <mkr4gkr@gmail.com>
2025-09-16 10:35:30 +0530

    Update main.tf

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -23,6 +23,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     content {
       log_analytics_workspace_id = var.log_analytics_workspace_id
   }
+ }
 
   role_based_access_control_enabled = true
  
